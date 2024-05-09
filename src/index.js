1bcc95b9ecff48a3dadb0609d544617cd30e2d40
souhailbakioui <bakiouisohail@gmail.com>
2024-05-09 23:32:12 +0100

    setup project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductList from './components/products/products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ProductList/>
   </React.StrictMode>
 );
 
